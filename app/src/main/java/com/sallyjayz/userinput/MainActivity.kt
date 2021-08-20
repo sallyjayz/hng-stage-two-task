e10214ee5514978064240d44dc23ee3c53ae27cb
sallyjayz <salama.jatau@yahoo.com>
2021-08-20 15:05:16 +0100

    made corrections on the task

--- app/src/main/java/com/sallyjayz/userinput/MainActivity.kt
+++ app/src/main/java/com/sallyjayz/userinput/MainActivity.kt
@@ -13,18 +13,10 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val zuriTextView = findViewById<MaterialTextView>(R.id.zuri_link_text)
-        val hngTextView = findViewById<MaterialTextView>(R.id.hng_link_text)
-        val i4gTextView = findViewById<MaterialTextView>(R.id.i4g_link_text)
-
-        zuriTextView.setTextColor(Color.BLUE)
-        hngTextView.setTextColor(Color.BLUE)
-        i4gTextView.setTextColor(Color.BLUE)
-
     }
 
     fun displayInput(view: View) {
@@ -42,20 +34,5 @@ class MainActivity : AppCompatActivity() {
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 
-    }
-
-    fun zuriLink(view: View) {
-        val zuriIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://zuri.team/"))
-        startActivity(zuriIntent)
-    }
-
-    fun hngLink(view: View) {
-        val hngIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://hng.tech/"))
-        startActivity(hngIntent)
-    }
-
-    fun i4gLink(view: View) {
-        val i4gIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://ingressive.org/"))
-        startActivity(i4gIntent)
     }
 }
